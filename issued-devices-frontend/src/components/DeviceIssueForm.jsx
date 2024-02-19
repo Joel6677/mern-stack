00934ca515a63a89dadc2a0dc9dbc79b440fc07c
Joel Heusala <joel.heusala@gmail.com>
2024-02-19 21:38:34 +0200

    form works but badly

--- issued-devices-frontend/src/components/DeviceIssueForm.jsx
+++ issued-devices-frontend/src/components/DeviceIssueForm.jsx
@@ -1,58 +1,63 @@
 import { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { useDispatch } from 'react-redux'
+import { createDevice } from '../reducers/deviceReducer'
+import {
+  useNavigate,
+} from "react-router-dom"
 
 
 const DeviceIssueForm = () => {
-  
-  const [newDevice, setNewDevice] = useState('')
 
-    const addDevice = (event) => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const addDevice = (event) => {
     event.preventDefault()
+
+    const formData = new FormData(event.target);
     const deviceObject = {
-      id: devices.length + 1,
-      name: newDevice,
-      manufacturer: 'Intel',
-      number: 123,
-      recipient_id: 1,
-      date_of_issue: new Date('03/25/2015'),
-      returning_date: new Date('04/10/2015')
-    }
+      name: formData.get('name'),
+      manufacturer: formData.get('manufacturer'),
+      number: formData.get('number'),
+      recipient: formData.get('recipientName'),
+      department: formData.get('department'),
+      date_of_issue: formData.get('dateOfIssue'),
+      returning_date: formData.get('returningDate')
+    };
 
-    setDevices(devices.concat(deviceObject))
-    setNewDevice('')
+    dispatch(createDevice(deviceObject));
+    event.target.reset(); 
+    navigate('/')
   }
 
-  const handleDeviceChange = (event) => {
-    console.log(event.target.value)
-    setNewDevice(event.target.value)
-  }
 
-    return (
-      <div style={{padding: 30}}>
-        <Form onSubmit={addDevice}>
-          <Form.Group className="mb-3" controlId="formDateOfIssue">
-            <Form.Label>Date of issue</Form.Label>
-            <Form.Control type="date" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formDeviceDetails">
-            <Form.Label>Device details</Form.Label>
-            <Form.Control type="text" placeholder="Name" />
-            <Form.Control type="text" placeholder="Manufacturer" />
-            <Form.Control type="number" placeholder="Device number" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formRecipientDetails">
-            <Form.Label>Recipient details</Form.Label>
-            <Form.Control type="text" placeholder="Name" />
-            <Form.Control type="text" placeholder="Department" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formDeviceDetails">
-            <Form.Label>Returning date</Form.Label>
-            <Form.Control type="date" />
-          </Form.Group>
-        </Form>
+  return (
+    <div style={{ padding: 30 }}>
+      <Form onSubmit={addDevice}>
+        <Form.Group className="mb-3" controlId="formDateOfIssue">
+          <Form.Label>Date of issue</Form.Label>
+          <Form.Control type="date" name="dateOfIssue" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formDeviceDetails">
+          <Form.Label>Device details</Form.Label>
+          <Form.Control type="text" placeholder="Name" name="name" />
+          <Form.Control type="text" placeholder="Manufacturer" name="manufacturer" />
+          <Form.Control type="number" placeholder="Device number" name="number" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formRecipientDetails">
+          <Form.Label>Recipient details</Form.Label>
+          <Form.Control type="text" placeholder="Name" name="recipientName" />
+          <Form.Control type="text" placeholder="Department" name="department" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formReturningDate">
+          <Form.Label>Returning date</Form.Label>
+          <Form.Control type="date" name="returningDate" />
+        </Form.Group>
         <Button type="submit">Submit</Button>
-      </div>
-      );
+      </Form>
+    </div>
+  )
 }
 
 export default DeviceIssueForm
--- issued-devices-frontend/src/services/devices.js
+++ issued-devices-frontend/src/services/devices.js
@@ -1,21 +1,17 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/devices'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
 }
 
-const create = newObject => {
-  return axios.post(baseUrl, newObject)
-}
-
-const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject)
+const createNew = async (newObject) => {
+  const response = await axios.post(baseUrl, newObject)
+  return response.data
 }
 
 export default { 
   getAll, 
-  create, 
-  update 
+  createNew
 }
