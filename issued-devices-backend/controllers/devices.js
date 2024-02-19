3e7fdda56897cfb4b1dd58293898e2ee28fca367
Joel Heusala <joel.heusala@gmail.com>
2024-02-19 05:09:22 +0200

    removing comments

--- issued-devices-backend/controllers/devices.js
+++ issued-devices-backend/controllers/devices.js
@@ -40,38 +40,4 @@ devicesRouter.post('/', async (request, response) => {
     }
   })
 
-// devicesRouter.get('/:id', (request, response, next) => {
-//     Device.findById(request.params.id)
-//         .then(device => {
-//             if (device) {
-//                 response.json(device)
-//             } else {
-//                 response.status(404).end()
-//             }
-//         })
-//         .catch(error => next(error))
-// })
-
-// devicesRouter.post('/', (request, response) => {
-
-//     const body = request.body
-
-//     if (body.name === undefined) {
-//         return response.status(400).json({ error: 'name missing' })
-//     }
-
-//     const device = new Device({
-//         name: body.name,
-//         manufacturer: body.manufacturer,
-//         number: body.number,
-//         recipient_id: body.recipient_id,
-//         date_of_issue: new Date(body.date_of_issue),
-//         returning_date: new Date(body.returning_date),
-//     })
-
-//     device.save().then(savedDevice => {
-//         response.json(savedDevice)
-//     }).catch(error => next(error))
-// })
-
 module.exports = devicesRouter
